8869dad5dca9f401cde0fb5fee44d7f1e4cb975e
Nils Wittler <nilswe@posteo.de>
2023-03-13 14:08:52 +0100

    update app assets

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,30 @@ export const metadata = {
     creator: "@nlswtlr",
     images: [{ url: "https://next-app-icons.vercel.app/assets/images/share/og-share.jpg", width: 1200, height: 630 }],
   },
+  icons: {
+    shortcut: {
+      url: "/favicon.ico",
+    },
+    icon: [
+      {
+        url: "/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+      {
+        url: "/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+    ],
+    apple: [
+      {
+        url: "/apple-touch-icon.png",
+        sizes: "180x180",
+      },
+    ],
+  },
+  manifest: "/manifest.webmanifest",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
